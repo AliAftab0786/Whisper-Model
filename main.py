9749917dad590d9264cbd6828b70458f75e2c183
AliAftab0786 <meharaliafatb0306@gmail.com>
2024-07-13 11:51:02 -0700

    All code

---
+++ main.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+api_key = "sk-proj-Ojh2CDkR50pv7fVLZ7nbT3BlbkFJ4RguyT3izMgrU55UbfFt"
+client = OpenAI(api_key=api_key)
+
+audio_file= open("./audio/test5.m4a", "rb")
+print("---------------------------------")
+print("Transcribing audio...")
+print("This may take a few Time.")
+print("Please wait...")
+print("---------------------------------")
+transcription = client.audio.transcriptions.create(
+  model="whisper-1", 
+  file=audio_file
+)
+print(transcription.text)
+
+with (open("./txt_files/speech_to_text_4.txt", "w" , encoding="utf-8")) as file:
+  file.write(transcription.text)
